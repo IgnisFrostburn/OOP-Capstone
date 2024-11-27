11654b0ea9bddee39529299464d4ab8c65a8dd73
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2024-11-27 15:32:23 +0800

    dashboard admin and student

---
+++ src/main/java/com/example/javafxdemo/DashboardStudent.java
@@ -0,0 +1,26 @@
+package com.example.javafxdemo;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DashboardStudent extends Application {
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(LoginPageApplication.class.getResource("DashboardStudent.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1080, 720);
+        stage.setTitle("Dashboard");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
